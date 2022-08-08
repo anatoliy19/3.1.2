dff3b7955115c1335c907e862b3aa14a31a25d73
Анатолий Андреев <anatoliiandreev19@gmail.com>
2022-08-09 00:25:14 +0300

    fix

--- src/main/java/ru/kata/spring/boot_security/demo/configs/DataInitializer.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/DataInitializer.java
@@ -31,41 +31,39 @@ public class DataInitializer {
         roleService.saveRole(role2);
 
         User user = new User();
-        user.setName("Anna");
-        user.setSurname("Muraveva");
-        user.setEmail("Test1@mail.ru");
-        user.setPassword("anna");
+        user.setName("Bob");
+        user.setSurname("Johnson");
+        user.setEmail("mail1@gmail.com");
+        user.setPassword("bob");
         user.addRole(role1);
 
-
         User user2 = new User();
-        user2.setName("Artem");
-        user2.setSurname("Muravev");
-        user2.setEmail("Test2@mail.ru");
-        user2.setPassword("artem");
+        user2.setName("Sam");
+        user2.setSurname("Dobkins");
+        user2.setEmail("mail2@gmail.com");
+        user2.setPassword("sam");
         user2.addRole(role1);
         user2.addRole(role2);
 
-
         User user3 = new User();
-        user3.setName("Ivan");
-        user3.setSurname("Ivanov");
-        user3.setEmail("Test3@mail.ru");
-        user3.setPassword("ivan");
+        user3.setName("Mike");
+        user3.setSurname("Trump");
+        user3.setEmail("mail3@gmail.com");
+        user3.setPassword("mike");
         user3.addRole(role2);
 
         User user4 = new User();
-        user4.setName("Petr");
-        user4.setSurname("Petrov");
-        user4.setEmail("Test4@mail.ru");
-        user4.setPassword("petr");
+        user4.setName("Ronald");
+        user4.setSurname("Mars");
+        user4.setEmail("mail4@gmail.com");
+        user4.setPassword("ronald");
         user4.addRole(role2);
 
         User user5 = new User();
-        user5.setName("Semen");
-        user5.setSurname("Kruglov");
-        user5.setEmail("Test5@mail.ru");
-        user5.setPassword("semen");
+        user5.setName("Donald");
+        user5.setSurname("Mask");
+        user5.setEmail("mail5@gmail.com");
+        user5.setPassword("donald");
         user5.addRole(role1);
         user5.addRole(role2);
 
